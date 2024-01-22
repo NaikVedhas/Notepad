caa0899f9bc36a2f11bd3c9541da4924625cd3c3
Vedhas Naik <145349104+NaikVedhas@users.noreply.github.com>
2024-01-22 18:47:14 +0530

    Update Project.sol

--- Project.sol
+++ Project.sol
@@ -1,1 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0 <0.9.0;
+
+contract local {
+    address owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    struct Users {
+        mapping(uint256 => string) data_store;
+        uint256 nth_data_store;
+    }
+
+    mapping(address => Users) public no_of_Users;
+    uint256 nth_user;
+
+    function add(string memory _data) public {
+        Users storage new_user = no_of_Users[msg.sender];
+        new_user.data_store[new_user.nth_data_store] = _data;
+        new_user.nth_data_store++;
+    }
+
+}
+
+
 
